14772255f3b99d3b963efccc7e1503aea165416b
Samy Bencherif <samybencherif@live.com>
2018-12-20 22:35:56 -0500

    fixed install script

--- language-support/install.sh
+++ language-support/install.sh
@@ -1,1 +1,1 @@
-cp -R /Users/samybencherif/Documents/Projects/teaspoon/language-support /Users/samybencherif/.vscode-insiders/extensions/teaspoon-language-support
+cp -R /Users/samybencherif/Documents/Projects/teaspoon/language-support/ /Users/samybencherif/.vscode-insiders/extensions/teaspoon-language-support
